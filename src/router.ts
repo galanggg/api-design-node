e87cf03cce00f55d4c52b8803fc793ffd54795c7
galanggg <tugasgalang@gmail.com>
2022-11-21 09:52:01 +0800

    feat: complete crud for api product

--- src/router.ts
+++ src/router.ts
@@ -1,29 +1,66 @@
-import {Router} from 'express';
+import { Router } from 'express'
+import { body, oneOf, validationResult } from 'express-validator'
+import {
+  createProduct,
+  deleteProduct,
+  getProduct,
+  getProducts,
+  updateProduct,
+} from './handlers/product'
+import { handleInputErrors } from './modules/middleware'
 const router = Router()
-/**Product
- *
- */
-router.get('/product', (req, res) => {
-    res.json({message: 'a'})
-})
-router.get('/product/:id', (req, res) =>{})
-router.put('/product/:id', (req, res) =>{})
-router.post('/product', (req, res) =>{})
-router.delete('/product/:id', (req, res) =>{})
+/**Product**/
+router.get('/product', getProducts)
+router.get('/product/:id', getProduct)
+router.put(
+  '/product/:id',
+  body('name').isString(),
+  handleInputErrors,
+  updateProduct,
+)
+router.post(
+  '/product',
+  body('name').isString(),
+  handleInputErrors,
+  createProduct,
+)
+router.delete('/product/:id', handleInputErrors, deleteProduct)
 
-/**Update */
-
+/**Update**/
 router.get('/update', (req, res) => {})
-router.get('/update/:id', (req, res) =>{})
-router.put('/update/:id', (req, res) =>{})
-router.post('/update', (req, res) =>{})
-router.delete('/update/:id', (req, res) =>{})
+router.get('/update/:id', (req, res) => {})
+router.put(
+  '/update/:id',
+  body('title').optional(),
+  body('body').optional(),
+  body('status').isIn(['IN_PROGRESS', 'LIVE', 'DEPRECATED', 'ARCHIVED']),
+  body('version').optional(),
+  (req, res) => {},
+)
+router.post(
+  '/update',
+  body('title').exists().isString(),
+  body('body').exists().isString(),
+  (req, res) => {},
+)
+router.delete('/update/:id', (req, res) => {})
 
-/**Update Point */
+/**Update Point**/
 router.get('/updatepoint', (req, res) => {})
-router.get('/updatepoint/:id', (req, res) =>{})
-router.put('/updatepoint/:id', (req, res) =>{})
-router.post('/updatepoint', (req, res) =>{})
-router.delete('/updatepoint/:id', (req, res) =>{})
+router.get('/updatepoint/:id', (req, res) => {})
+router.put(
+  '/updatepoint/:id',
+  body('name').optional().isString(),
+  body('description').optional().isString(),
+  (req, res) => {},
+)
+router.post(
+  '/updatepoint',
+  body('name').optional().isString(),
+  body('description').optional().isString(),
+  body('updateId').exists().isString(),
+  (req, res) => {},
+)
+router.delete('/updatepoint/:id', (req, res) => {})
 
 export default router
