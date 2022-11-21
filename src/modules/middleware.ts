153fc52ab5b28b1ad6ddc6779633dda649c9b1a0
galanggg <tugasgalang@gmail.com>
2022-11-21 09:52:38 +0800

    feat: add middleware

---
+++ src/modules/middleware.ts
@@ -0,0 +1,16 @@
+import { validationResult } from 'express-validator'
+import { Request, Response, NextFunction } from 'express'
+
+export const handleInputErrors = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    res.status(400)
+    res.json({ errors: errors.array() })
+  } else {
+    next()
+  }
+}
