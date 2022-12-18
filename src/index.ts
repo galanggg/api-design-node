41520b4c29b043b63769abb978b6db5e7d11f9f7
galanggg <tugasgalang@gmail.com>
2022-12-18 15:20:27 +0800

    feat: add new env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import app from './server'
+import config from './config'
 
-
-app.listen(3001, () => {
-    console.log('server running on localhost:3001')
+app.listen(config.port, () => {
+  console.log(`server running on localhost:${config.port}`)
 })
